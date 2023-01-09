99983c1304abd74741eb838148cbdb95af9a42dc
todoggler <kentokuraCS@gmail.com>
2023-01-09 23:15:16 +0900

    refactor(main): generateNonceをファイル分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
-String generateNonce([int length = 32]) {
-  const charset =
-      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-  final random = Random.secure();
-  final randomStr =
-      List.generate(length, (_) => charset[random.nextInt(charset.length)])
-          .join();
-  return randomStr;
-}
+import './utils/generateNonce.dart';
 
 void main() {
   runApp(
