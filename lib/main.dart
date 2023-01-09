962e11d862eb6ec2ec641a94933dcc10ef6990c7
todoggler <kentokuraCS@gmail.com>
2023-01-09 20:21:36 +0900

    feat(main): 色々変更してWidgetを切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -38,56 +38,7 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               for (int i = 0; i <= 100; i++) ...{
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://api.lorem.space/image/game?w=100&h=100'),
-                      ),
-                      const SizedBox(width: 8),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: const [
-                              Text('ken-ty@Flutter大学'),
-                              SizedBox(width: 8),
-                              Text(
-                                '2022/05/05',
-                                style: TextStyle(color: Colors.black54),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 4),
-                          Text(generateNonce(min(30, Random().nextInt(100)))),
-                          Row(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.chat_bubble_outline),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.repeat),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.favorite_border),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.ios_share),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                const TweetTitle(),
               }
             ],
           ),
@@ -99,7 +50,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Text(
-//         'Hello, world!',
-//         textDirection: TextDirection.ltr,
-//       ),
+class TweetTitle extends StatelessWidget {
+  const TweetTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+            backgroundImage:
+                NetworkImage('https://api.lorem.space/image/game?w=100&h=100'),
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: const [
+                  Text('ken-ty@Flutter大学'),
+                  SizedBox(width: 8),
+                  Text(
+                    '2022/05/05',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Text(generateNonce(min(30, Random().nextInt(100)))),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.chat_bubble_outline),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.repeat),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.ios_share),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
